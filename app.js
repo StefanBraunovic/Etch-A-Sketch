7f02461d90ff0b4bc342882d049072dbae3cce55
Stefan Braunovic <Stefan.braunovic55@gmail.com>
2020-05-13 13:58:34 +0200

    some buttons

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-let container = document.querySelector('.container')
+const container = document.querySelector('.container')
 const black = document.querySelector("#black");
-
+const reset = document.getElementById('reset')
+const size = document.getElementById('size')
+const rnb = document.getElementById('rnb')
 
 
 
@@ -12,23 +14,51 @@ function makeGrid(cols, rows){
         container.appendChild(pixel).classList.add('gridPixel');
          }
         }
+        makeGrid(16,16)
 
         function blackColor() {
             let cells = document.querySelectorAll('.gridPixel');
             
             cells.forEach(div => {
 
-                div.addEventListener("mouseover", () => {
-                    event.target.style.backgroundColor = "black";
+                div.addEventListener("mouseover", (e) => {
+                    e.target.style.backgroundColor = "black";
                 });
             });
+            }
+
+            function rnbColor(){
+                let cells = document.querySelectorAll('.gridPixel');
             
+            cells.forEach(div => {
+
+                div.addEventListener("mouseover", (e) => {
+                    e.target.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);;
+                });
+            });
+            }
+
+    function grdiSize(){
+             var x = parseFloat(prompt("Enter a Value", "0"));
+            var y = parseFloat(prompt("Enter a Value", "0"));
+        if(x&&y<=64){
+            makeGrid(x,y)
+        }else{
+            alert("It's too big, Aaaah")
         }
+     }
+    function refresh(){
+        location.reload();
+return false;
+    }
+
+
+rnb.addEventListener("click",rnbColor);       
+reset.addEventListener("click",refresh);        
+black.addEventListener("click", blackColor)
+size.addEventListener("click",grdiSize);         
         
-         
-        black.addEventListener("click", blackColor)        
-        
- makeGrid(16,16)
+ 
 
 
 
